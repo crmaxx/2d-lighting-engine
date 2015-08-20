771c1b12c35a0e21c2553a1a865fcd7c8c5016b8
J Luke <git@whitevoidia.me>
2015-08-19 20:16:13 -0500

    Remove Image.h/cpp. Closes #2

--- lightingengine/lightingengine/src/Image.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-#include <SDL2/SDL.h>
-#include <SDL/SDL_image.h>
-
-SDL_Texture *loadTexture(SDL_Renderer*, std::string);
-void renderTexture(SDL_Renderer*, SDL_Texture*, int, int);
